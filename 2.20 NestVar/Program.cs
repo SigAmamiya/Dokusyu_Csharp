5c8f4346f48393ed73c8d530daf7d6fadb027bce
Masato Narita <Das_hexenhaus@hotmail.co.jp>
2018-01-29 13:41:28 +0900

    ブロックをネストした際中と外で同じ変数名を宣言できないサンプルプログラムを追加

---
+++ 2.20 NestVar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NestVar
+{
+    class Program
+    {
+        static void Main()
+        {
+            int count;
+
+            for (count = 0; count < 10; count = count + 1) {
+                Console.WriteLine("Count is :" + count);
+
+                int count;  //countはすでにMain()で宣言されているためここでは宣言できない。
+                for (count = 0; count < 2; count++) {
+                    Console.WriteLine("This Program is in error!");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
