aab23ad52038e1392c2be0e1aae032e2c0c671b3
Masato Narita <Das_hexenhaus@hotmail.co.jp>
2018-01-29 13:03:34 +0900

    スコープのデモプログラムを追加

---
+++ 2.10 ScopeDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace ScopeDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x;
+
+            x = 10;
+
+            if (x == 10) {
+                int y = 20; //yはifの中のブロックでのみ有効
+                Console.WriteLine("x and y:" + x + " " + y);
+                x = y * 2; //ここではまだ有効
+
+            }
+
+           // y = 100; //コンパイルエラー　ここからはyにアクセスできない
+
+            Console.WriteLine("x is " + x );//xはifの中のブロックではないのでここでは有効
+            Console.ReadKey();
+
+        }
+    }
+}
