184c8ac9a39100cb7e63af246093f55e9a6f01f6
Masato Narita <Das_hexenhaus@hotmail.co.jp>
2018-01-29 13:58:41 +0900

    剰余のサンプルプログラム

---
+++ 2.12 ModDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace ModDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            int iresult, irem;
+            double dresult, drem;
+
+            iresult = 10 / 3;
+            irem = 10 % 3;
+
+            dresult = 10.0 / 3.0;
+            drem = 10.0 % 3.0;
+            //%では剰余が計算できるため、int型でもdouble型でも1が算出される。
+
+            Console.WriteLine("Result and remainder of 10 / 3: " + iresult + " " + irem);
+            Console.WriteLine("Result and remainder of 10.0 / 3.0: " + dresult + " " + drem);
+
+            Console.ReadKey();
+
+        }
+    }
+}
