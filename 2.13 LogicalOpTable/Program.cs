d5456362056b27a2197b85afd45443ad7595c124
Masato Narita <Das_hexenhaus@hotmail.co.jp>
2018-01-29 15:45:40 +0900

    論理演算子の真偽表のプログラム

---
+++ 2.13 LogicalOpTable/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace LogicalOpTable
+{
+    class LogicalOpTable
+    {
+        static void Main()
+        {
+            bool p, q;
+            
+
+            //AND 両方true  ならtrue
+            //OR　片方true  ならtrue
+            //XOR 片方だけtrue ならtrue
+            //NOT 逆の結果
+
+            Console.WriteLine("P\tQ\tAND\tOR\tXOR\tNOT");
+
+            p = true; q = true;
+            Console.Write(p + "\t" + q + "\t");
+            Console.Write((p & q) + "\t" + (p | q) + "\t");
+            Console.WriteLine((p ^ q) + "\t" + (!p));
+
+            p = true; q = false;
+            Console.Write(p + "\t" + q + "\t");
+            Console.Write((p & q) + "\t" + (p | q) + "\t");
+            Console.WriteLine((p ^ q) + "\t" + (!p));
+
+            p = false; q = true;
+            Console.Write(p + "\t" + q + "\t");
+            Console.Write((p & q) + "\t" + (p | q) + "\t");
+            Console.WriteLine((p ^ q) + "\t" + (!p));
+
+            p = false; q = false;
+            Console.Write(p + "\t" + q + "\t");
+            Console.Write((p & q) + "\t" + (p | q) + "\t");
+            Console.WriteLine((p ^ q) + "\t" + (!p));
+
+            Console.ReadKey();
+
+        }
+    }
+}
