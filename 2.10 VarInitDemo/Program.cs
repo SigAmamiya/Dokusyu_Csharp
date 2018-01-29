c1a1ad0fc7934b1536fb9d8d20d581c773223f78
Masato Narita <Das_hexenhaus@hotmail.co.jp>
2018-01-29 13:28:59 +0900

    変数宣言に初期化子が含まれる場合、その変数が宣言されたブロックに処理が入るたびに変数が再度初期化されるプログラム

---
+++ 2.10 VarInitDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace VarInitDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x;
+
+            for (x = 0; x < 3; x++) {
+                int y = -1;
+                Console.WriteLine("y is :" + y);//ループされるたびにyが初期化されるため-1と表示される
+
+                y = 100;
+                Console.WriteLine("y is now :" + y);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
