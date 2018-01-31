6cd6fd7bfa378ba1770c67e54e1bbc3ca9837553
Masato Narita <Das_hexenhaus@hotmail.co.jp>
2018-01-31 10:42:46 +0900

    暗黙的な型の変換のプログラム

---
+++ 2.14 LtoD/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LtoD
+{
+    class LtoD
+    {
+        static void Main()
+        {
+            long L;
+            double D;
+
+            L = 100123285L;
+            D = L; //long型からdouble型に暗黙的な自動変換が行われる
+
+            Console.WriteLine("L and D:" + L + " " + D);
+            Console.ReadKey();
+
+        }
+    }
+}
