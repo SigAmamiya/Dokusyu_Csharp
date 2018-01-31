024d00f22211941de853702a8e410fdaf8d5f16f
Masato Narita <Das_hexenhaus@hotmail.co.jp>
2018-01-31 11:44:56 +0900

    式における型変換のサンプルプログラム

---
+++ 2.17 PromDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PromDemo
+{
+    class PromDemo
+    {
+        static void Main()
+        {
+            byte b;
+            int i;
+
+            b = 10;
+            i = b * b;
+
+            b = 10;
+            b =(byte)(b * b);
+
+            Console.WriteLine("i and b: " + i + " " + b);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
