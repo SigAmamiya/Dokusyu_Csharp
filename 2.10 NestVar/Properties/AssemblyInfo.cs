0dc3a6efe8ab2922a9376d79c8559fc7ee30fe6c
Masato Narita <Das_hexenhaus@hotmail.co.jp>
2018-01-31 12:36:48 +0900

    2.10 NestVerの参照先を修正

--- 2.10 NestVar/Properties/AssemblyInfo.cs
+++ 2.10 NestVar/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("2.20 NestVar")]
+[assembly: AssemblyTitle("2.10 NestVar")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("2.20 NestVar")]
+[assembly: AssemblyProduct("2.10 NestVar")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
