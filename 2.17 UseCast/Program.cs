fe9ea66b16cd488ccfc60abc6d30cf6eee7bd8f7
Masato Narita <Das_hexenhaus@hotmail.co.jp>
2018-01-31 11:55:53 +0900

    除算での浮動小数点へのキャストを利用したサンプルプログラム

---
+++ 2.17 UseCast/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UseCast
+{
+    class UseCast
+    {
+        static void Main()
+        {
+            int i;
+
+            for(i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i + " / 3:" + i / 3);
+                Console.WriteLine(i + " / 3 with fanctions: {0:#.##} ", (double)i / 3);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
